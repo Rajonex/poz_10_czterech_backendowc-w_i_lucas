0073e0340669126ff354f88a0c74982d8759b2f7
kuklix <szymonkukla1@gmail.com>
2019-04-13 04:49:02 +0200

    photos for Bartek :>

---
+++ spring/src/main/java/com/example/spring/model/OfferImages.java
@@ -0,0 +1,15 @@
+package com.example.spring.model;
+
+import java.util.List;
+
+public class OfferImages {
+    List<String> url;
+
+    public List<String> getUrl() {
+        return url;
+    }
+
+    public void setUrl(List<String> url) {
+        this.url = url;
+    }
+}
